f0effc6757e73a271e457df3e5135414c1626a46
Steven Black <sblack@us.ibm.com>
2016-08-26 00:11:14 -0400

    Namespaces the app and baked in jQuery & Lodash

--- src/js/base.js
+++ src/js/base.js
@@ -1,13 +1,18 @@
 /*	Simple Cycle Base JS - Huzzah!
 ------------------------------------------*/
 
+	window.sc = {};
+
 	/*	Libraries
 	------------------------------------------*/
 		var $ = require("jquery"),
 			_ = require("lodash");
 
+		sc.$ = $;
+		sc._ = _;
+
 		// Make lodash templates use mustache style
-		_.templateSettings = {
+		sc._.templateSettings = {
 			evaluate:    /\{\[([\s\S]+?)\]\}/g,
 			interpolate: /{{([\s\S]+?)}}/g,
 			escape:      /{{{([\s\S]+?)}}}/g
